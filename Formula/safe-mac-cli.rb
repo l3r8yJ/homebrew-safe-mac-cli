5be106a09206ce4c23fde79ecb12bd54d0a9f7ee
Ivan <clicker.heroes.acg@gmail.com>
2022-11-29 18:58:49 +0300

    sha

--- Formula/safe-mac-cli.rb
+++ Formula/safe-mac-cli.rb
@@ -2,7 +2,7 @@ class SafeMacCli < Formula
   desc "A CLI tool, that creates new .env file which based on something you choose and appends a mac-address to it."
   homepage "https://github.com/l3r8yJ/safe-mac-cli"
   url "https://github.com/l3r8yJ/safe-mac-cli/releases/download/0.1.3/safe-mac-cli-0.1.3-apple-silicon.tar.gz"
-  sha256 "64e02ce4f580fb1bd4d4a80a6f15d21a526f788c"
+  sha256 "0c87bc450b7695ec610d0b20f85c63b5e83f2765f3ec60c86d56ec1519563d6b"
   version "0.1.3"
 
   def install
